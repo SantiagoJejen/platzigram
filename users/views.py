888f4df04c56368cf457d5e4a5fa2ddbe97abc4b
Santiago Jejen <santiagojejens@gmail.com>
2020-12-27 12:54:24 -0500

    First update form

--- users/views.py
+++ users/views.py
@@ -6,13 +6,15 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 
+
 # Exception
 from django.db.utils import IntegrityError
 
 # Models
 from django.contrib.auth.models import User
 from users.models import Profile
-
+# Forms
+from users.forms import ProfileForm
 
 def login_view(request):
     """Login view."""
@@ -64,5 +66,31 @@ def logout_view(request):
     return redirect('login')
 
 def update_profile(request):
-    """update  a user's profile view"""
-    return render(request, 'users/update_profile.html')
+    """Update a user's profile view."""
+    profile = request.user.profile
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            profile.website = data['website']
+            profile.phone_number = data['phone_number']
+            profile.biography = data['biography']
+            profile.picture = data['picture']
+            profile.save()
+
+            return redirect('update_profile')
+
+    else:
+        form = ProfileForm()
+
+    return render(
+        request=request,
+        template_name='users/update_profile.html',
+        context={
+            'profile': profile,
+            'user': request.user,
+            'form': form
+        }
+    )
