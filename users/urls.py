9795b55cbafa36fbf88750fb0a394946fdff8f95
Santiago Jejen <santiagojejens@gmail.com>
2020-12-31 20:38:23 -0500

    Create file whit urls for this app

---
+++ users/urls.py
@@ -0,0 +1,35 @@
+"""Posts URLs."""
+
+# Django
+from django.urls import path
+from django.views.generic import TemplateView
+
+# Views
+
+from users import views 
+
+urlpatterns= [
+
+    path(
+        route='<str:username>/',
+        view=TemplateView.as_view(template_name='users/detail.html'),
+        name ='detail'
+    ),
+
+    path(
+        route='login/',
+        view=views.login_view, 
+        name = 'login'),
+    path(
+        route='logout/',
+        view=views.logout_view, 
+        name = 'logout'),
+    path(
+        route='signup/',
+        view=views.signup, 
+        name = 'signup'),
+    path(
+        route='me/profile/',
+        view=views.update_profile, 
+        name = 'update_profile'),
+]
